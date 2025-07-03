5384b8639283e918180f982676c9f8a897ed2106
ishandeep48 <ishandeep48@gmail.com>
2025-07-03 21:48:44 +0530

    Just fixed some styles

--- P2P-sharing/src/pages/Home.jsx
+++ P2P-sharing/src/pages/Home.jsx
@@ -9,7 +9,12 @@ function Home(){
     return(
         <div>
             <NavBar/>
-            <h1>This is a Home page</h1>
+            <h1>P2P File Sharing</h1>
+            <h3>This is a Server Aided Peer to Peer File Sharing Website. The Transfer Rate of the files 
+                depends on your network connection not on the Server . The Server can be bad but your transfer 
+                rate will still be amazing if you have good connection. I made this as a fun side kinda project just 
+                to share files with my friends in my college hostel .......................      ( also i dont know what else to make ) 
+                I WANT INTERNSHIP 😭😭😭😭</h3>
             
         </div>
     );
